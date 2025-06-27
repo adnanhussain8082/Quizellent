4d5276de3c807aa6994a967463022d4d276378aa
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-06-27 23:09:29 +0530

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,17 @@
+class c
+{
+    //class A
+}
+
+class B
+{
+    //class B
+}
+
+class A
+{
+    public static void main(String args[])
+    {
+        System.out.println("Inside C class");
+    }
+}
